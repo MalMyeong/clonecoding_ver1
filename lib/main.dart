be2db7411e33132efedf02106aca2547b21fe71f
MalMyeong <audgus4321@gmail.com>
2023-05-09 21:29:19 +0900

    make Column

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter'),
-        ),
-        body: Center(
-          child: Text('Hello world'),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [Row()],
         ),
       ),
     );
